ffedd57c9e0426399a76db859fa20e5f8451a0f8
Dominik Mierzejewski <dominik.mierzejewski1@wp.pl>
2025-01-14 20:43:35 +0100

    push

--- backend_gradebook/dist/src/server.js
+++ backend_gradebook/dist/src/server.js
@@ -45,7 +45,7 @@ const server = http_1.default.createServer(app);
 exports.server = server;
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://gradebookexams-1.onrender.com',
         methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
         credentials: true,
         allowedHeaders: ['Content-Type', 'Authorization']
@@ -54,7 +54,7 @@ const io = new socket_io_1.Server(server, {
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:5173',
+    origin: 'https://gradebookexams-1.onrender.com',
     credentials: true,
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ['Content-Type', 'Authorization']
