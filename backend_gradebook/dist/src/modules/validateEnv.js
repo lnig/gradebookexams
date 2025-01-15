5ecf1d54d59d002be04a5cd28f4fa70fca79a0a5
Dominik Mierzejewski <dominik.mierzejewski1@wp.pl>
2025-01-16 00:39:54 +0100

    correct

--- backend_gradebook/dist/src/modules/validateEnv.js
+++ backend_gradebook/dist/src/modules/validateEnv.js
@@ -7,13 +7,12 @@ const SECRET_KEY = process.env.SECRET_KEY;
 exports.SECRET_KEY = SECRET_KEY;
 const DATABASE_URL = process.env.DATABASE_URL;
 exports.DATABASE_URL = DATABASE_URL;
-const DATABASE_TEST_URL = process.env.DATABASE_TEST_URL;
 const SMTP_USER = process.env.SMTP_USER;
 exports.SMTP_USER = SMTP_USER;
 const SMTP_PASS = process.env.SMTP_PASS;
 exports.SMTP_PASS = SMTP_PASS;
 const BEARER_TOKEN = process.env.BEARER_TOKEN;
 exports.BEARER_TOKEN = BEARER_TOKEN;
-if (!PORT || !SECRET_KEY || !DATABASE_URL || !DATABASE_TEST_URL || !SMTP_USER || !SMTP_PASS || !BEARER_TOKEN) {
+if (!PORT || !SECRET_KEY || !DATABASE_URL || !SMTP_USER || !SMTP_PASS || !BEARER_TOKEN) {
     throw new Error('Missing required environment variables. Please check your .env file.');
 }
