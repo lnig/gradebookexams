c2dce2e8a95f34356f97bc09f1e5b2fe3beb3015
Dominik Mierzejewski <dominik.mierzejewski1@wp.pl>
2025-01-16 00:44:14 +0100

    correct

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,2 @@
-export const API_GRADEBOOK_URL = 'https://gradebook-1-s3gw.onrender.com';
+export const API_GRADEBOOK_URL = 'https://gradebook-kdlv.onrender.com';
 export const API_EXAMS_URL = 'https://exams-v1a7.onrender.com';
